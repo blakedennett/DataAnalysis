6f25426b45fc6c6fe2b05f50a56e35df86bdacea
blakedennet2 <den18019@byui.edu>
2023-05-26 11:07:08 -0600

    got it to work on play.py

--- play.py
+++ play.py
@@ -6,3 +6,20 @@ print(word[-4:])
 
 print(word[:-1])
 
+
+states = [25, 4]
+
+
+predictions = []
+
+dict_1 = {25:[1,2,3], 4:[3,2,19]}
+
+for i in range(len(dict_1[25])):
+    num = 0
+    for s in states:
+        num += dict_1[s][i]
+    predictions.append(num/len(states))
+    num = 0
+
+for p in predictions:
+    print(p)
