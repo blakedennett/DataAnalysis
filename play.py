7542b52b3f2c4e0f3ec175c91b374f3f1f4f5552
blakedennet2 <den18019@byui.edu>
2023-05-30 15:44:20 -0600

    found the high and low are based on day

--- play.py
+++ play.py
@@ -13,9 +13,9 @@ states = [25, 4]
 predictions = []
 
 dict_1 = {25:[1,2,3], 4:[3,2,19]}
-
+num = 0
 for i in range(len(dict_1[25])):
-    num = 0
+    
     for s in states:
         num += dict_1[s][i]
     predictions.append(num/len(states))
